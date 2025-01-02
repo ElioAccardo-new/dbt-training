9743d379f9f3405400fe14f15af717624edbc119
Elio Accardo <elio.accardo@dell.com>
2025-01-02 20:45:42 +0000

    report models

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,24 @@
+SELECT 
+-- from raw orders
+       o.orderid,
+       o.orderdate,
+       o.shipdate,
+       o.shipmode,
+       o.ordersellingprice - o.ordercostprice as orderprofit,
+       o.ordercostprice,
+       o.ordersellingprice,
+-- from raw customer
+       c.customerid,
+       c.customername,
+       c.segment,
+       c.country,
+-- from raw_product
+       p.category,
+       p.productid,
+       p.productname,
+       p.subcategory 
+from {{ ref('raw_orders') }} o
+left join {{ ref('raw_customer') }} c 
+    on o.customerid=c.customerid
+left join {{ ref('raw_product') }} p
+    on o.productid=p.productid
